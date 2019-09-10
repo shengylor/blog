8dad619b491d3cc86d668c3b5e341351b589c052
sheng <shengy@gmail.com>
2019-09-10 13:26:04 -0500

    Added login and logout system. Also added profile page

--- users/views.py
+++ users/views.py
@@ -7,10 +7,12 @@ def register(request):
 		form = UserRegisterForm(request.POST)
 		if form.is_valid():
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}!')
-			return redirect('blog-home')	
+			messages.success(request, f'Your account has been created! {username}!')
+			return redirect('login')	
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
 
+def profile(request):
+	return render(request, 'users/profile.html')
